f57c19b5cbb9126f64d1a9ec8d5104f0fde212a5
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-06-05 13:11:47 -0700

    Finished skeletal structure on flask survery program

--- Python/python_stack/flask_fundamentals/dojo-survey/server.py
+++ Python/python_stack/flask_fundamentals/dojo-survey/server.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, redirect
-
-app=Flask(__name__)
+from flask import Flask, render_template, redirect, request
+# I spend a lot of time on this. Formatting can still be updated to smooth out the inputs and 
+app= Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 @app.route('/results', methods=['POST'])
-def results():
-    name = request.form['name']
-    comment = request.form['comment']
-    return render_template('results.html', name = name, comment = comment)
+def showResults():
+    result = {}
+    result['name']=request.form['name']
+    result['city']=request.form['city']
+    result['language']=request.form['language']
+    result['comment']=request.form['comment']
+    return render_template('results.html', result=result)
+
 app.run(debug=True)
