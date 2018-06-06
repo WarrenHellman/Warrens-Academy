5b07666bf5378792a229fff2be4074bb6fa188ee
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-06-06 12:46:47 -0700

    Finished Missing Ninjas Exercise

---
+++ Python/python_stack/flask_fundamentals/missing-turtles/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, request
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ninja')
+def ninja():
+    return render_template('ninja.html', color="")
+@app.route('/ninja/<color>')
+def color(color=None):
+    print color
+    return render_template('ninja.html', color=color)
+
+app.run(debug=True)
