d0eccde2f5f9a3923e90ccd8a5ec0a6828c41d59
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-06-07 14:57:11 -0700

    finished Ninja Gold Exercise

---
+++ Python/python_stack/flask_fundamentals/ninja-gold/server.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, redirect, request, session
+
+app=Flask(__name__)
+app.secret_key = 'ThisIsSecret'
+
+import random
+import datetime
+
+@app.route('/')
+def index():
+    session['gold']=0
+    session['activity']=""
+    return render_template('index.html', gold=session['gold'])
+
+@app.route('/process_money', methods=['POST'])
+def process():
+    # doesn't add to new line each output
+    if request.form['building']=='farm':
+        val = random.randrange(10,21)
+        session['gold']+=val
+        activity= "-Earned "+str(val)+" gold from the farm! "+str(datetime.datetime.now())
+        session['activity']+=activity
+    elif request.form['building']=='cave':
+        val = random.randrange(5,11)
+        session['gold']+=val
+        activity= "-Earned "+str(val)+" gold from the cave! "+str(datetime.datetime.now())
+        session['activity']+=activity
+    elif request.form['building']=='house':
+        val = random.randrange(2,6)
+        session['gold']+=val
+        activity= "-Earned "+str(val)+" gold from the house! "+str(datetime.datetime.now())
+        session['activity']+=activity
+    else:
+        val = random.randrange(-50,51)
+        session['gold']+=val
+        if val>0:
+            activity= "-Went into the casino and won "+str(val)+" gold! Yes!"+str(datetime.datetime.now())
+            session['activity']+=activity
+        elif val<0:
+            val*=-1
+            activity= "-Went into the casino and lost "+str(val)+" gold! Yikes!"+str(datetime.datetime.now())
+            session['activity']+=activity
+        else:
+            activity= "-Went into the casino and left with what you came with. Could have been worse!"+str(datetime.datetime.now())
+            session['activity']+=activity
+    return render_template('index.html', gold=session['gold'], activity=session['activity'])
+
+app.run(debug=True)
