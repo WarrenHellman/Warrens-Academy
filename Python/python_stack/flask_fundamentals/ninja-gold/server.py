9c5a9794099e9340a453b9e0afc0a9a76bf87688
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-06-11 10:36:57 -0700

    finished Ninja Gold to add scrolling activity log and conditional red/green casino entries

--- Python/python_stack/flask_fundamentals/ninja-gold/server.py
+++ Python/python_stack/flask_fundamentals/ninja-gold/server.py
@@ -9,41 +9,39 @@ import datetime
 @app.route('/')
 def index():
     session['gold']=0
-    session['activity']=""
+    session['activity']=[]
     return render_template('index.html', gold=session['gold'])
 
 @app.route('/process_money', methods=['POST'])
 def process():
-    # doesn't add to new line each output
-    
     if request.form['building']=='farm':
         val = random.randrange(10,21)
-        session['gold']+=val
+        session['gold']=random.randrange(10,21)
         activity= "-Earned "+str(val)+" gold from the farm! "+str(datetime.datetime.now())
-        session['activity']+=activity
+        session['activity'].append(activity)
     elif request.form['building']=='cave':
         val = random.randrange(5,11)
         session['gold']+=val
         activity= "-Earned "+str(val)+" gold from the cave! "+str(datetime.datetime.now())
-        session['activity']+=activity
+        session['activity'].append(activity)
     elif request.form['building']=='house':
         val = random.randrange(2,6)
         session['gold']+=val
         activity= "-Earned "+str(val)+" gold from the house! "+str(datetime.datetime.now())
-        session['activity']+=activity
+        session['activity'].append(activity)
     else:
         val = random.randrange(-50,51)
         session['gold']+=val
         if val>0:
-            activity= "-Went into the casino and won "+str(val)+" gold! Yes!"+str(datetime.datetime.now())
-            session['activity']+=activity
+            activity= "-Went into the casino and won "+str(val)+" gold! Yes! "+str(datetime.datetime.now())
+            session['activity'].append(activity)
         elif val<0:
             val*=-1
-            activity= "-Went into the casino and lost "+str(val)+" gold! Yikes!"+str(datetime.datetime.now())
-            session['activity']+=activity
+            activity= "-Went into the casino and lost "+str(val)+" gold! Yikes! "+str(datetime.datetime.now())
+            session['activity'].append(activity)
         else:
-            activity= "-Went into the casino and left with what you came with. Could have been worse!"+str(datetime.datetime.now())
-            session['activity']+=activity
-    return render_template('index.html', gold=session['gold'], activity=activity)
+            activity= "-Went into the casino and left with what you came with. Could have been worse! "+str(datetime.datetime.now())
+            session['activity'].append(activity)
+    return render_template('index.html', gold=session['gold'], activity=session['activity'])
 
 app.run(debug=True)
