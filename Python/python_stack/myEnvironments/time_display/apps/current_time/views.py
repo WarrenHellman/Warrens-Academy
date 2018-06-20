435e0315915483b4ef4f0baa49e44a981a0f9f5f
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-06-20 10:57:48 -0700

    Finished current time app

--- Python/python_stack/myEnvironments/time_display/apps/current_time/views.py
+++ Python/python_stack/myEnvironments/time_display/apps/current_time/views.py
@@ -2,8 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse, redirect
-
+from time import gmtime, strftime, localtime
 # Create your views here.
 def index(request):
-    response = "time placeholder"
-    return HttpResponse(response)
+    # response = "time placeholder"
+    context = {
+        'time1': strftime("%Y-%m-%d %H:%M %p", gmtime()),
+        'time2': strftime("%Y-%m-%d %H:%M %p", localtime())
+    }
+    return render(request, 'current_time/index.html', context)
