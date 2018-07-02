93b7d6cd550aaeba8a0594022241f20cd6931960
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-07-02 14:28:31 -0700

    User Dashboard- added remove, edit, admin dash

--- Python/python_stack/myEnvironments/dashboard/apps/dash/models.py
+++ Python/python_stack/myEnvironments/dashboard/apps/dash/models.py
@@ -2,33 +2,33 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import bcrypt
 
 # Create your models here.
 class UserManager(models.Manager):
     def validator(self, postData):
         errors = {}  
-        # if len(postData['first'])<3:
-        #     errors['first'] = 'First name must contain at least 3 characters'
-        # if len(postData['last']) <3:
-        #     errors['last'] = 'Last name must contain at least 3 characters'
-        # if User.objects.filter(email=postData['email']):
-        #     errors['email'] = 'That email address is already registered'
-        # if postData['password']!= postData['confirm']:
-        #     errors['password'] = 'Passwords do not match'
-        # if len(postData['password'])<8:
-        #     errors['password'] = 'Password must be at least 8 characters'
+        if len(postData['first'])<3:
+            errors['first'] = 'First name must contain at least 3 characters'
+        if len(postData['last']) <3:
+            errors['last'] = 'Last name must contain at least 3 characters'
+        if User.objects.filter(email=postData['email']):
+            errors['email'] = 'That email address is already registered'
+        if postData['pwd']!= postData['conpwd']:
+            errors['password'] = 'Passwords do not match'
+        if len(postData['pwd'])<8:
+            errors['pwd'] = 'Password must be at least 8 characters'
         return errors
         
     def logvalidator(self, postData):
         errors = {}
-        # if not User.objects.filter(email=postData['log-email']):
-        #     errors['log-email'] = 'That email address has not been registered'
-        # user = self.filter(email=postData['log-email'])
+        if not User.objects.filter(email=postData['email']):
+            errors['email'] = 'That email address has not been registered'
+        user = self.filter(email=postData['email'])
 
-        # uncomment below
-        # for word in user:
-            # if not bcrypt.checkpw(postData['password'].encode(), word.password.encode()):
-            #     errors['password']='Passwords do not match'
+        for word in user:
+            if not bcrypt.checkpw(postData['pwd'].encode(), word.password.encode()):
+                errors['pwd']='Passwords do not match'
         
         return errors
 
@@ -37,8 +37,10 @@ class User(models.Model):
     first = models.CharField(max_length=100)
     last = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=50)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
-    user_level = None
-    
+    user_level = models.IntegerField(null=True)
+    description = models.CharField(max_length=500)
+
     objects = UserManager()
--- Python/python_stack/myEnvironments/dashboard/apps/dash/views.py
+++ Python/python_stack/myEnvironments/dashboard/apps/dash/views.py
@@ -4,30 +4,83 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from .models import User
 from django.contrib import messages
+import bcrypt
 
 # Create your views here.
 def index(request):
     return render(request, 'dash/index.html')
 
 def signin(request):
+    
     return render(request, 'dash/signin.html')
+
+def passcheck(request):
+    errors = User.objects.logvalidator(request.POST)
+    if len(errors):
+        for tag, error in errors.iteritems():
+            messages.error(request, error, extra_tags=tag)
+        return redirect('/signin')
+    
+    else:
+        user = User.objects.get(email=request.POST['email'])
+        if user.user_level == 9:
+            return redirect('/dashboard/admin')
+        else: 
+            return redirect('/dashboard')
+
+def dashAdmin(request):
+    context = {
+        'users':User.objects.all()
+    }
+    return render(request, 'dash/dashAdmin.html', context)
 
 def register(request):
     return render(request, 'dash/register.html')
 
 def dashboard(request):
-    return render(request, 'dash/dashboard.html')
+    context = {
+        'users':User.objects.all()
+    }
+    return render(request, 'dash/dashboard.html', context)
 
-def newUser(request):
-    # Add flash messages
-    user = User.objects.create()
-    id = user.id
+def edit(request, id):
+    context= {
+        'user': User.objects.get(id=id)
+    }    
+    return render(request, 'dash/edit.html', context)
+def remove(request, id):
+    user = User.objects.get(id=id)
+    user.delete()
+    return redirect('/dashboard/admin')
+
+def update(request, id):
+    user = User.objects.get(id=id)
+    user.email = request.POST['email']
     user.first = request.POST['first']
     user.last = request.POST['last']
-    user.email = request.POST['email']
-    user.password = request.POST['pwd']
-    
     user.save()
+    return redirect('/dashboard/admin')
 
-    return redirect('/', {'user':User.objects.get(id=id)})
+def newUser(request):
+    errors = User.objects.validator(request.POST)
+    if len(errors):
+        for tag, error in errors.iteritems():
+            messages.error(request, error, extra_tags=tag)
+        return redirect('/register')
+    else:
+        user = User.objects.create()
+        id = user.id
+        user.email = request.POST['email']
+        user.first = request.POST['first']
+        user.last = request.POST['last']
+        user.password = request.POST['pwd']
+        hashed = bcrypt.hashpw(user.password.encode(), bcrypt.gensalt())
+        user.password = hashed
+        if user.id == 1:
+            user.user_level = 9
+        else: 
+            user.user_level = 1
+        user.save()
+
+        return redirect('/dashboard', {'user':User.objects.get(id=id)})
 
