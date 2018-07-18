d471163c25848edc24c7b1322d96ec3b5cfc37f4
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-07-18 14:16:06 -0700

    File moving

--- Udemy/Modern Javascript/js_playground/app.js
+++ Udemy/Modern Javascript/js_playground/app.js
@@ -1,1 +1,24 @@
-console.log('working')
+// PRIMITIVE
+
+// STRING
+const name = 'John Doe';
+// NUM
+const age = 45
+// Boolean
+const hasKids=true;
+// null
+const car = null;
+// undefined
+let test;
+// Symbol, new primative type
+const sym = Symbol()
+
+// REFERENCE TYPE (object)
+// Array
+const hobbies = ['movies', 'music']
+// Object Literal
+const address = {
+    city: 'Boston',
+    state: 'MA'
+}
+const today = new Date();
