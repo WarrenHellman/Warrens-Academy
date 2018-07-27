37f751fb6de300c33f05b3d63db276ba7d185160
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-07-27 15:08:52 -0700

    Modern JS Selectors

---
+++ Udemy/Modern Javascript/Section 3- Manipulating the DOM/Notes and practice.js
@@ -0,0 +1,45 @@
+// ################################
+// #######Accessing the DOM########
+// ################################
+
+//let val;
+
+// val = document;
+// val = document.all;
+// // returns an html collection, arraylike and comma separated
+// val = document.all[3];
+// val = document.head;
+// val = document.body;
+// val = document.doctype;
+// val = document.domain;
+// val = document.URL;
+// val = document.characterSet;
+// val = document.contentType;
+
+// val = document.forms;
+// val = document.forms[0];
+// val = document.forms[0].id;
+// val = document.forms[0].method;
+// val = document.forms[0].action;
+
+// val = document.links;
+// val = document.links[0];
+// val = document.links[0].className;
+// val = document.links[0].classList;
+
+// val = document.images;
+
+// val = document.scripts;
+// val = document.scripts[0];
+// val = document.scripts[2].getAttribute('src');
+
+// let scripts = document.scripts;
+
+// //need to arrayify the HTML collection
+// let scriptsArr = Array.from(scripts);
+
+// scriptsArr.forEach(function(script) {
+//   console.log(script.getAttribute('src'));
+// });
+
+// console.log(val);
--- Udemy/Modern Javascript/Section 3- Manipulating the DOM/app.js
+++ Udemy/Modern Javascript/Section 3- Manipulating the DOM/app.js
@@ -1,40 +1,91 @@
-let val;
+//Select item
 
-val = document;
-val = document.all;
-val = document.all[2];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+// console.log(document.getElementById('task-title'))
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+//Get Things from the Element
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList[0];
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
 
-val = document.images;
+//Change Styling
 
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
+// document.getElementById('task-title').style.background = '#333';
+// document.getElementById('task-title').style.color = '#fff';
+// document.getElementById('task-title').style.padding = '5px';
+// document.getElementById('task-title').style.display = 'none';
 
-let scripts = document.scripts;
+//Change Content
 
-let scriptsArr = Array.from(scripts);
+//easier to set a variable.
+const taskTitle = document.getElementById('task-title');
+//both change the displayed content in between the tags
+taskTitle.textContent = 'Task List';
+taskTitle.innerText = 'My Tasks';
+taskTitle.innerHTML = '<span style="color:red">Task List</span>';
 
-scriptsArr.forEach(function(script) {
-  console.log(script.getAttribute('src'));
-});
+//document.querySelector()- a single selector
 
-console.log(val);
+//works with any css selector
+
+// console.log(document.querySelector('#task-title'));
+// console.log(document.querySelector('.card-title'));
+// console.log(document.querySelector('h5'));
+
+// document.querySelector('li').style.color = 'red';
+// document.querySelector('ul li').style.color = 'red';
+// document.querySelector('li:last-child').style.color = 'red';
+// document.querySelector('li:nth-child(3)').style.color = 'yellow';
+// document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+
+
+// Selecting Multiple Elements
+
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'red';
+// items[3].textContent = 'Hello';
+
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+
+// console.log(listItems)
+
+//document.getElementsByTagName - selects by tag instead of class or id
+let lis = document.getElementsByTagName('li');
+// console.log(lis);
+// console.log(lis[0]);
+
+// To do array methods, turn into array
+lis = Array.from(lis);
+
+// lis.reverse();
+
+// lis.forEach(function(li, index){
+//   console.log(li.className)
+//   li.textContent = `${index}: Hello`;
+// })
+
+//document.querySelectorAll()- returns node list which can do more array methods without conversion
+
+const items = document.querySelectorAll('ul.collection li.collection-item');
+
+// items.forEach(function(item, index){
+//   console.log(item.className)
+//   item.textContent = `${index}: Hello`;
+// })
+
+const liOdd = document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)');
+
+// Can iterate through the items with querySelectorAll
+liOdd.forEach(function(li){
+  li.style.background = '#ccc'
+})
+
+// Or with a for loop
+
+for(let i=0;i<liEven.length;i++){
+  liEven[i].style.background = '#f4f4f4'
+};
+
+console.log(items)
