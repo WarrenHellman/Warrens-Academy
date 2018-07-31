6e8e98ed06830c30811d2a4ed5bc5e13ff3e2622
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-07-31 14:47:10 -0700

    Modern JS dom traversal and node creation

--- Udemy/Modern Javascript/Section 3- Manipulating the DOM/Notes and practice.js
+++ Udemy/Modern Javascript/Section 3- Manipulating the DOM/Notes and practice.js
@@ -43,3 +43,197 @@
 // });
 
 // console.log(val);
+
+// ################################
+// #######DOM Selectors############
+// ################################
+
+//Select item
+
+// console.log(document.getElementById('task-title'))
+
+//Get Things from the Element
+
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
+
+//Change Styling
+
+// document.getElementById('task-title').style.background = '#333';
+// document.getElementById('task-title').style.color = '#fff';
+// document.getElementById('task-title').style.padding = '5px';
+// document.getElementById('task-title').style.display = 'none';
+
+//Change Content
+
+//easier to set a variable.
+const taskTitle = document.getElementById('task-title');
+//both change the displayed content in between the tags
+taskTitle.textContent = 'Task List';
+taskTitle.innerText = 'My Tasks';
+taskTitle.innerHTML = '<span style="color:red">Task List</span>';
+
+//document.querySelector()- a single selector
+
+//works with any css selector
+
+// console.log(document.querySelector('#task-title'));
+// console.log(document.querySelector('.card-title'));
+// console.log(document.querySelector('h5'));
+
+// document.querySelector('li').style.color = 'red';
+// document.querySelector('ul li').style.color = 'red';
+// document.querySelector('li:last-child').style.color = 'red';
+// document.querySelector('li:nth-child(3)').style.color = 'yellow';
+// document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+
+
+// Selecting Multiple Elements
+
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'red';
+// items[3].textContent = 'Hello';
+
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+
+// console.log(listItems)
+
+//document.getElementsByTagName - selects by tag instead of class or id
+let lis = document.getElementsByTagName('li');
+// console.log(lis);
+// console.log(lis[0]);
+
+// To do array methods, turn into array
+lis = Array.from(lis);
+
+// lis.reverse();
+
+// lis.forEach(function(li, index){
+//   console.log(li.className)
+//   li.textContent = `${index}: Hello`;
+// })
+
+//document.querySelectorAll()- returns node list which can do more array methods without conversion
+
+const items = document.querySelectorAll('ul.collection li.collection-item');
+
+// items.forEach(function(item, index){
+//   console.log(item.className)
+//   item.textContent = `${index}: Hello`;
+// })
+
+const liOdd = document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)');
+
+// Can iterate through the items with querySelectorAll
+liOdd.forEach(function(li){
+  li.style.background = '#ccc'
+})
+
+// Or with a for loop
+
+for(let i=0;i<liEven.length;i++){
+  liEven[i].style.background = '#f4f4f4'
+};
+
+console.log(items)
+
+// ################################
+// #######Traversing the DOM#######
+// ################################
+
+let val;
+
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
+
+val = listItem;
+val = list;
+
+//Get Child Nodes
+val = list.childNodes;//gives the items as well as some text nodes (in this case it is just the space in between nodes)
+val = list.childNodes[0];
+val = list.childNodes[0].nodeName;
+val = list.childNodes[1].nodeType;
+
+//--Node Types--
+//1 - Element
+//2 - Attribute (deprecated)
+//3 - Text Node
+//8 - Comment
+//9 - Document itself
+//10 - Doctype
+
+
+// Get teh children element nodes
+// val = list.children; // gives an htmlcollection of just the children elements
+
+val = list.children[0];
+val = list.children[1].textContent = 'Hell';
+val = list.children[3].children[0].id = 'test-link';
+
+// First Child
+val = list.firstChild; //gives first child regardless
+val = list.firstElementChild; //gives first element
+
+// Last Child
+val = list.lastChild;
+val = list.lastElementChild;
+
+// Get Parent Node
+val = listItem.parentNode; //Ul
+val = listItem.parentElement.parentElement; //can get grandparents!
+
+//Get Sibling
+
+val = listItem.nextSibling; //in this case, text node
+val = listItem.nextElementSibling;
+
+val = listItem.previousSibling;
+val = listItem.previousElementSibling;
+
+console.log(val);
+
+
+
+
+// ################################
+// #######Creating Elements########
+// ################################
+
+
+//Create Element
+
+const li = document.createElement('li');
+
+//Add Class
+li.className = 'collection-item';
+
+//Add id
+li.id = 'new-item';
+
+//Add attribute
+li.setAttribute('title', 'New Item');
+
+// Create text node and append
+
+li.appendChild(document.createTextNode('Hello World'));
+
+//create new link element
+const link = document.createElement('a');
+//add classes
+link.className= 'delete-item secondary-content';
+
+//Add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>';
+
+//Append link into
+li.appendChild(link);
+
+// Append li as childe to ul
+document.querySelector('ul.collection').appendChild(li);
+
+
+console.log(li);
