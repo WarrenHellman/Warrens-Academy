42d7ef579bdd6a4828afd63b946a180545c20d01
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-08-01 15:12:22 -0700

    Modern JS Local and Session Storage

--- Udemy/Modern Javascript/Section 3- Manipulating the DOM/Notes and practice.js
+++ Udemy/Modern Javascript/Section 3- Manipulating the DOM/Notes and practice.js
@@ -446,3 +446,47 @@ function deleteItem(e){
       e.target.parentElement.parentElement.remove();
   }
 }
+
+// ################################
+// #Session and local storage######
+// ################################
+
+//Set local storage item
+// localStorage.setItem('name', 'Warren')
+
+//Set session storage item
+// sessionStorage.setItem('name', 'Erin')
+
+//remove from storage
+// localStorage.removeItem('name')
+
+//get from storage
+// const name = localStorage.getItem('name');
+// const age = localStorage.getItem('age')
+
+//clear local storage
+// localStorage.clear();
+// console.log(name);
+
+document.querySelector('form').addEventListener('submit', function(e){
+  const task = document.getElementById('task').value;
+
+  let tasks;
+  if (localStorage.getItem('tasks')===null){
+      tasks =[];
+  }
+  else {
+      tasks=JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+  e.preventDefault();
+});
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+tasks.forEach(function(task){
+  console.log(tasks)
+})
