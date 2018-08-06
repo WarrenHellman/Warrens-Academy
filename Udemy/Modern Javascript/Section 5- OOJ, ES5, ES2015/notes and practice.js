4fcc63c6531d49c13f9b38b60fed67a509001941
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-08-06 12:25:04 -0700

    Modern JS Prototypic Inheritance

--- Udemy/Modern Javascript/Section 5- OOJ, ES5, ES2015/notes and practice.js
+++ Udemy/Modern Javascript/Section 5- OOJ, ES5, ES2015/notes and practice.js
@@ -110,3 +110,46 @@ warren.getsMarried('Korte')
 console.log(warren.getFullName());
 
 console.log(warren.hasOwnProperty('middleName'))
+
+// ##################################
+// #####Prototypical Inheritance#####
+// ##################################
+
+function Person(firstName, lastName){
+  this.firstName= firstName;
+  this.lastName= lastName;
+}
+
+//Greeting
+Person.prototype.greeting = function(){
+  return `Hello there ${this.firstName} ${this.lastName}`;
+}
+
+const person1 = new Person('John', 'Doe');
+// console.log(person1.greeting())
+
+function Customer(firstName, lastName, phone, membership) {
+  
+  //calls another constructor for info
+  Person.call(this, firstName, lastName);
+  this.phone = phone;
+  this.membership = membership;
+}
+
+//Inherit the Person prototype methods
+Customer.prototype = Object.create(Person.prototype);
+
+//Make customer.prototype return Customer()
+Customer.prototype.constructor= Customer;
+
+// Create Customer
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard')
+
+console.log(customer1);
+
+//Overwrite inherited greeting
+Customer.prototype.greeting = function(){
+  return `Hello there ${this.firstName} ${this.lastName}, welcome to the company`;
+}
+
+console.log(customer1.greeting());
