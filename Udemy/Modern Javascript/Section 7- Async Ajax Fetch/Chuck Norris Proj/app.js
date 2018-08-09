b81a1ab66b235b3b17c656bec671031b8ac45aa7
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-08-09 10:07:43 -0700

    Finished Chuck Norris Generator

--- Udemy/Modern Javascript/Section 7- Async Ajax Fetch/Chuck Norris Proj/app.js
+++ Udemy/Modern Javascript/Section 7- Async Ajax Fetch/Chuck Norris Proj/app.js
@@ -4,7 +4,26 @@ function getJokes(e){
   const number = document.querySelector('input[type="number"]').value;
 
   const xhr = new XMLHttpRequest();
+  // Should be working, behind SBP wifi network it won't allow the redirects
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true)
 
-  xhr.open(`http://api.icndb.com/jokes/random/${number}`)
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
+
+      let output = '';
+      
+      if (response.type === 'success') {
+        response.value.forEach(function(joke){
+          output += `<li>${joke.joke}</li>`
+        });
+      } else {
+        output += '<li>Something went wrong</li>'
+      }
+      document.querySelector('.jokes').innerHTML = output;
+    }
+  }
+
+  xhr.send();
   e.preventDefault();
 }
