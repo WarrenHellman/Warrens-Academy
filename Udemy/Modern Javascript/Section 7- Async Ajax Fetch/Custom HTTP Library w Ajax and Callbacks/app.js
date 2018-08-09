fe1ff8097e9a2ff8bb97a1d809c1b49f8c731fb0
WarrenHellman <34872725+WarrenHellman@users.noreply.github.com>
2018-08-09 14:33:39 -0700

    Put, Delete, Post methods for library

--- Udemy/Modern Javascript/Section 7- Async Ajax Fetch/Custom HTTP Library w Ajax and Callbacks/app.js
+++ Udemy/Modern Javascript/Section 7- Async Ajax Fetch/Custom HTTP Library w Ajax and Callbacks/app.js
@@ -1,11 +1,62 @@
 const http = new easyHTTP;
-//Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
+
+//Get All Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+  
+// });
+
+// Get single post
+
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, posts){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+  
+// });
+
+// Create Data
+
+const data = {
+  title: 'Custome Post',
+  body: 'this is a custom post',
+  userID: 49494
+}
+
+//Create Post
+
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
+//   if (err){
+//         console.log(err);
+//       } else {
+//         console.log(post);
+//       }
+// });
+
+// Update Post
+
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post){
   if (err){
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(post);
+  }
+})
+
+
+//Delete REquest
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response){
+  if (err){
+    console.log(err);
+  } else {
+    console.log(response);
   }
   
 });
-
